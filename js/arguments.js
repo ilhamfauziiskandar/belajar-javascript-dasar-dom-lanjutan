2d340d31d6352e535b4163aff5a18786271c060c
ilham fauzi iskandar <ilhamfauzyiskandar@gmail.com>
2021-02-28 21:15:23 +0700

    sec commit

---
+++ js/arguments.js
@@ -0,0 +1,7 @@
+//-------- argument --------
+function data(){
+    return arguments;
+}
+
+var coba = data(1,2,3,4,5, false, "goblok");
+console.log(coba);
--- js/fungsi.js
+++ js/fungsi.js
@@ -1,30 +1,22 @@
 // -------- function -------
-// function volume2kubus(a, b) {
-//     var volume1 = a * a * a;
+function volume2kubus(a, b) {
+    var volume1 = a * a * a;
     
-//     var volume2 = b * b * b;
+    var volume2 = b * b * b;
     
-//     var volumekubus = volume1 + volume2;
+    var volumekubus = volume1 + volume2;
     
-//     return volumekubus;
-// }
-
-// var q = confirm("Apakah anda ingin mencoba rumus Volume 2 kubus ?");
-
-// if (q == true) {
-//     var a = parseInt(prompt("masukan nilai sisi kubus peratama"));
-    
-//     var b = parseInt(prompt("masukan nilai sisi kubus kedua"));
-    
-//     console.log(volume2kubus(a, b));
-    
-//     alert('nilai volume dari kedua kubus adalah ' + volume2kubus(a, b));
-// }
-
-//-------- argument --------
-function data(){
-    return arguments;
+    return volumekubus;
 }
 
-var coba = data(1,2,3,4,5, false, "goblok");
-console.log(coba);
+var q = confirm("Apakah anda ingin mencoba rumus Volume 2 kubus ?");
+
+if (q == true) {
+    var a = parseInt(prompt("masukan nilai sisi kubus peratama"));
+    
+    var b = parseInt(prompt("masukan nilai sisi kubus kedua"));
+    
+    console.log(volume2kubus(a, b));
+    
+    alert('nilai volume dari kedua kubus adalah ' + volume2kubus(a, b));
+}
