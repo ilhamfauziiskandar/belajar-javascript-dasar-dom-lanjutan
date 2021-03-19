1dc0d0578864b5d12618f8e0c5f74c9e7c56da10
ilham fauzi iskandar <ilhamfauzyiskandar@gmail.com>
2021-03-19 21:33:28 +0700

    latihan Bermain Warna

--- js/js-dom/dom-bermainWarna.js
+++ js/js-dom/dom-bermainWarna.js
@@ -12,12 +12,16 @@ const slideBiru = document.querySelector('input[name="sliderBiru"]');
 
 const slideHijau = document.querySelector('input[name="sliderHijau"]');
 
+const inputanMerah = document.querySelector('input[name="inputanMerah"]');
+
+const inputanBiru = document.querySelector('input[name="inputanBiru"]');
+
+const inputanHijau = document.querySelector('input[name="inputanHijau"]');
+
 btnAcakAngka.addEventListener('click', function() {
     
     const pAcakAngka = document.querySelectorAll("p.acakWarna");
 
-    const spanAngkaKehokian = document.querySelectorAll("span.angkaKehokian")
-    
     if (pAcakAngka.length < 1) {
         const pBaru = document.createElement("p");
     
@@ -43,29 +47,17 @@ btnAcakAngka.addEventListener('click', function() {
 
         divBody[0].appendChild(spanBaru);
     }else{
-        const pBaru = document.createElement("p");
-    
-        pBaru.classList.add("acakWarna");
-    
+        const pAcak = document.querySelector('p.acakWarna');
+        
+        const spanKehokian = document.querySelector('span.angkaKehokian');
+
         const angkaAcak = Math.round(Math.random() * 100);
-    
-        const pTextBaru = document.createTextNode(angkaAcak); 
-    
-        pBaru.appendChild(pTextBaru);
-    
-        divBody[0].replaceChild(pBaru, pAcakAngka[0]);
+
+        pAcak.innerHTML = angkaAcak;
 
         let kehokianAnda = kehokian(angkaAcak);
-        
-        const spanBaru = document.createElement("span");
-        
-        const textSpan = document.createTextNode(kehokianAnda);
 
-        spanBaru.classList.add("angkaKehokian")
-        
-        spanBaru.appendChild(textSpan);
-
-        divBody[0].replaceChild(spanBaru, spanAngkaKehokian[0]);
+        spanKehokian.innerHTML = kehokianAnda;
     }
 
 });
@@ -78,6 +70,18 @@ btnAcakRGB.addEventListener("click", function(){
     const blue = Math.round(Math.random() * 255);
     
     document.body.style.backgroundColor = 'rgb('+ red +','+ green +','+ blue +')';
+
+    inputanMerah.value = red;
+
+    inputanBiru.value = blue;
+
+    inputanHijau.value = green;
+
+    slideMerah.value = red;
+
+    slideBiru.value = blue;
+    
+    slideHijau.value = green;
 })
 
 slideMerah.addEventListener("change", function(){
@@ -89,9 +93,7 @@ slideMerah.addEventListener("change", function(){
 
     document.body.style.backgroundColor = 'rgb('+ red +','+ green +','+ blue +')';
 
-    const pWarnaMerah = document.querySelector("p.merah");
-    
-    pWarnaMerah.innerHTML = red;
+    inputanMerah.value = red;
 })
 
 slideBiru.addEventListener("change", function(){
@@ -103,9 +105,8 @@ slideBiru.addEventListener("change", function(){
 
     document.body.style.backgroundColor = 'rgb('+ red +','+ green +','+ blue +')';
     
-    const pWarnaBiru = document.querySelector("p.biru");
-    
-    pWarnaBiru.innerHTML = blue;
+    inputanBiru.value = blue;
+
 })
 
 slideHijau.addEventListener("change", function(){
@@ -117,9 +118,46 @@ slideHijau.addEventListener("change", function(){
 
     document.body.style.backgroundColor = 'rgb('+ red +','+ green +','+ blue +')';
 
-    const pWarnaHijau = document.querySelector("p.hijau");
+    inputanHijau.value = green;
+})
+
+inputanMerah.addEventListener("change", function(){
+    const red = inputanMerah.value;
     
-    pWarnaHijau.innerHTML = green;
+    const blue = inputanBiru.value;
+    
+    const green = inputanHijau.value;
+
+    document.body.style.backgroundColor = 'rgb('+ red +','+ green +','+ blue +')';
+
+    slideMerah.value = red;
+
+})
+
+inputanBiru.addEventListener("change", function(){
+    const red = inputanMerah.value;
+    
+    const blue = inputanBiru.value;
+    
+    const green = inputanHijau.value;
+
+    document.body.style.backgroundColor = 'rgb('+ red +','+ green +','+ blue +')';
+
+    slideBiru.value = blue;
+
+})
+
+inputanHijau.addEventListener("change", function(){
+    const red = inputanMerah.value;
+    
+    const blue = inputanBiru.value;
+    
+    const green = inputanHijau.value;
+
+    document.body.style.backgroundColor = 'rgb('+ red +','+ green +','+ blue +')';
+
+    slideHijau.value = green;
+
 })
 
 function kehokian(angkaHoki) {
