9518b603d024d23942c6210a9dea9a3f21d7ce96
ilham fauzi iskandar <ilhamfauzyiskandar@gmail.com>
2021-03-21 21:45:49 +0700

    latihan 2 suwit

--- js/js-dom/dom-suwit.js
+++ js/js-dom/dom-suwit.js
@@ -1,13 +1,17 @@
 const suwitAlert = document.querySelector("button.suwitAlert");
 
+const pilihanSuwitJawa = document.querySelectorAll("ul li img");
+
 suwitAlert.addEventListener("click", function mulaiSuwitAlert(){
     let pilihan = pilihPilihan();
 
     if (pilihan == undefined) {
         pilihan = pilihPilihan();
+    }else if(pilihan == false){
+        return;
     }
 
-    const comp = pilihanComputer();
+    const comp = getpilihanComputer();
     
     const hasil = hasilSuwit(pilihan, comp);
 
@@ -20,9 +24,33 @@ suwitAlert.addEventListener("click", function mulaiSuwitAlert(){
     }    
 })
 
+pilihanSuwitJawa.forEach(function(pilihan){
+    pilihan.addEventListener("click", function(){
+        const pilihanPlayer = pilihan.className;
+        
+        const pilihanComputer = getpilihanComputerJawa();
+
+        const hasil = hasilSuwitJawa(pilihanPlayer,pilihanComputer);
+
+        putarSuwitJawa();
+        
+        setTimeout(function() {
+            const imgComputer = document.querySelector("img.img-komputer");
+        
+            imgComputer.setAttribute('src', '../img/img/'+ pilihanComputer + '.png');
+            
+            const info = document.querySelector("div.info");
+            
+            info.innerHTML = hasil;    
+        }, 2000);
+
+        
+    })
+})
+
 function pilihPilihan() {
     let pilihanAnda = prompt("Gunting, Kertas, atau Batu");
-    
+    console.log(pilihanAnda)
     const pilihan =
     {
         gunting: "gunting",
@@ -40,35 +68,62 @@ function pilihPilihan() {
         }else{
             alert("anda memilih pilhan yang salah");
         }
+    }else if (pilihanAnda == "") {
+        pilihanAnda = undefined;
+        alert("anda memilih pilhan yang salah");
+        return pilihanAnda;
     }else{
+        pilihanAnda = false;
         alert("anda telah berhenti bermain");
+        return pilihanAnda;
     }
 }
 
-function pilihanComputer(){
+function getpilihanComputerJawa(){
     let comp = Math.random();
     
-    if (comp < 0.34) {
-        comp = "gunting";
-    }else if (comp >= 0.34 && comp < 0.67){
-        comp = "kertas"
-    }else{
-        comp = "batu"
-    }
-    return comp;
+    if (comp < 0.34) return "orang";
+    if (comp >= 0.34 && comp < 0.67) return "gajah";
+    return "semut";
+}
+
+function getpilihanComputer(){
+    let comp = Math.random();
+    
+    if (comp < 0.34) return "gunting";
+    if (comp >= 0.34 && comp < 0.67) return "kertas";
+    return "batu";
 }
 
 function hasilSuwit(pilihan, comp){
-    let hasil;
     
-    if (pilihan == comp) {
-        hasil = "seri";
-    }else if(pilihan == "batu"){
-        hasil = ( comp == "gunting") ? "menang" : "kalah";
-    }else if(pilihan == "gunting"){
-        hasil = ( comp == "kertas") ? "menang" : "kalah";
-    }else if(pilihan == "kertas"){
-        hasil = ( comp == "batu") ? "menang" : "kalah";
-    }
-    return hasil;
+    if (pilihan == comp) return "seri";
+    if (pilihan == "batu") return ( comp == "gunting") ? "menang" : "kalah";
+    if (pilihan == "gunting") return ( comp == "kertas") ? "menang" : "kalah";
+    if (pilihan == "kertas") return( comp == "batu") ? "menang" : "kalah";
+    
+}
+
+function hasilSuwitJawa(pilihan, comp){
+    
+    if (pilihan == comp) return "seri";
+    if (pilihan == "orang") return ( comp == "semut") ? "menang" : "kalah";
+    if (pilihan == "gajah") return ( comp == "orang") ? "menang" : "kalah";
+    if (pilihan == "semut") return( comp == "gajah") ? "menang" : "kalah";
+    
+}
+
+function putarSuwitJawa(){
+    const imgComputer = document.querySelector("img.img-komputer");
+    const gambar = ['gajah','semut','orang'];
+    let i = 0;
+    const waktuMulai = new Date().getTime();
+    setInterval(function(){
+        if(new Date().getTime() - waktuMulai > 2000){
+            clearInterval;
+            return;
+        }
+        imgComputer.setAttribute('src', '../img/img/' + gambar[i++] + '.png');
+        if( i == gambar.length) i = 0;
+    }, 100)
 }
